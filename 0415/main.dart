89fa84b91d9565dbcce06b64d25f5509e97622b0
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-04-15 23:12:29 +0900

    Create main.dart

---
+++ 0415/main.dart
@@ -0,0 +1,21 @@
+// main.dart
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Health Tracker',
+      home: HealthRecordPage(),
+    );
+  }
+}
